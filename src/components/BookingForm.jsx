64ddfb3c8ccfdd88c59d776d60542be091b47ca0
vishnuvardhanreddy12344 <vishnureddymachireddy@gmail.com>
2024-01-31 21:56:35 +0530

    second commit

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -38,7 +38,7 @@ const BookingForm = () => {
       <h1 className="text-4xl font-bold mb-6">Book Tickets for {show && show.name}</h1>
       <form onSubmit={handleSubmit} className="bg-white p-6 rounded-md shadow-md">
         <div className="mb-4">
-          <label htmlFor="userName" className="block text-gray-700 text-sm font-semibold mb-2">
+          <label htmlFor="userName" className="block text-gray-600 text-sm font-semibold mb-2">
             Your Name
           </label>
           <input
